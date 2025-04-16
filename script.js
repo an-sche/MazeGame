3b74751f1e2f3794d6e19880321fa84f7454c77d
an-sche <bscheit92@gmail.com>
2025-04-16 00:25:41 -0400

    Cleanup files, add mazes.js to separate maze functions

--- script.js
+++ script.js
@@ -1,107 +1,64 @@
+import { mazes, generateSeenArray } from './mazes.js'
 
-const maze1 = [
-  [1,1,1,1,1,1,1,1,1,1],
-  [1,0,0,0,1,0,0,0,0,1],
-  [1,0,1,0,1,0,1,1,0,1],
-  [1,0,1,0,0,0,0,1,0,1],
-  [1,0,1,1,1,1,0,1,0,1],
-  [1,0,0,0,0,1,0,1,0,1],
-  [1,1,0,1,0,1,0,1,0,1],
-  [1,0,0,1,0,1,0,1,0,1],
-  [1,0,1,1,0,0,0,1,0,1],
-  [1,1,1,1,1,1,1,1,1,1],
-];
-
-const maze2 = [
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,0,0,0,1,0,0,0,0,1,0,0,0,0,1],
-  [1,0,1,0,1,0,1,1,0,1,0,1,1,0,1],
-  [1,0,1,0,0,0,0,1,0,0,0,0,1,0,1],
-  [1,0,1,1,1,1,0,1,1,1,1,0,1,0,1],
-  [1,0,0,0,0,1,0,0,0,0,1,0,1,0,1],
-  [1,1,1,1,0,1,1,1,1,0,1,1,1,0,1],
-  [1,0,0,1,0,0,0,0,1,0,0,0,1,0,1],
-  [1,0,1,1,0,1,1,0,1,1,1,0,1,0,1],
-  [1,0,1,0,0,0,1,0,0,0,1,0,1,0,1],
-  [1,0,1,0,1,0,1,1,1,0,1,1,1,0,1],
-  [1,0,0,0,1,0,0,0,1,0,0,0,0,0,1],
-  [1,1,1,0,1,1,1,0,1,1,1,1,1,0,1],
-  [1,0,0,0,0,0,1,0,0,0,0,0,1,0,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-];
-
-let seen = generateSeenArray(10, 10);
+let currentMazeIndex = -1 // start at -1 because we add 1 when generating next maze
 let playerPos = { x: 1, y: 1 };
 let goal = { x: 8, y: 8 };
-
-let mazeData = maze1;
-
+let mazeData = [];
+let seen = [];
 const mazeEl = document.getElementById("maze");
 
+generateNextMaze();
+
 function drawMaze() {
-  mazeEl.innerHTML = "";
-  for (let y = 0; y < mazeData.length; y++) {
-    for (let x = 0; x < mazeData[y].length; x++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      if (mazeData[y][x] === 1 && seen[y][x] === 1) cell.classList.add("wall");
-      else if (goal.x === x && goal.y === y) cell.classList.add("goal");
-      if (playerPos.x === x && playerPos.y === y) cell.classList.add("player");
-      mazeEl.appendChild(cell);
+    mazeEl.innerHTML = "";
+    for (let y = 0; y < mazeData.length; y++) {
+        for (let x = 0; x < mazeData[y].length; x++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            if (mazeData[y][x] === 1 && seen[y][x] === 1) cell.classList.add("wall");
+            else if (goal.x === x && goal.y === y) cell.classList.add("goal");
+            if (playerPos.x === x && playerPos.y === y) cell.classList.add("player");
+            mazeEl.appendChild(cell);
+        }
     }
-  }
 }
 
 function movePlayer(dx, dy) {
-  const newX = playerPos.x + dx;
-  const newY = playerPos.y + dy;
-  if (mazeData[newY][newX] === 0) {
-    for (let y = newY - 1; y <= newY + 1; y++) {
-        for (let x = newX - 1; x <= newX + 1; x++) {
-            seen[y][x] = 1;
+    const newX = playerPos.x + dx;
+    const newY = playerPos.y + dy;
+    if (mazeData[newY][newX] === 0) {
+        for (let y = newY - 1; y <= newY + 1; y++) {
+            for (let x = newX - 1; x <= newX + 1; x++) {
+                seen[y][x] = 1;
+            }
         }
-    }
-    playerPos = { x: newX, y: newY };
+        playerPos = { x: newX, y: newY };
 
-    if (playerPos.x === goal.x && playerPos.y === goal.y) {
-        // we reset the game here... 
-        setupMazeData(2);
-        return;
+        if (playerPos.x === goal.x && playerPos.y === goal.y) {
+            // we reset the game here... 
+            generateNextMaze();
+            return;
+        }
+        drawMaze();
     }
-    drawMaze();
-  }
 }
 
-function generateSeenArray(width, height) {
-  let result = [];
-  for (let y = 0; y < height; y++) {
-    result[y] = [];
-    for (let x = 0; x < width; x++) {
-        if (x === 0 || y === 0 || x === width - 1 || y === height - 1 || (x < 3 && y < 3)) {
-            result[y][x] = 1;
-        } else {
-            result[y][x] = 0;
-        }
-    }
-  }
-  return result;
-}
 
-function setupMazeData(mazeIndex) {
-    playerPos = { x: 1, y: 1};
+function generateNextMaze() {
+    playerPos = { x: 1, y: 1 };
 
-    if (mazeIndex === 1) {
-        mazeData = maze1;
-    } else {
-        mazeData = maze2;
-    }
+    currentMazeIndex = (currentMazeIndex + 1) % mazes.length;
+    mazeData = mazes[currentMazeIndex];
+
+    const mazeTitleEl = document.getElementById("maze-title");
+    mazeTitleEl.textContent = "Maze: " + (currentMazeIndex + 1);
 
     const height = mazeData.length;
     const width = mazeData[0].length;
 
     const mazeElement = document.getElementById("maze");
-    mazeElement.style.gridTemplateColumns = `repeat(${width}, 40px)`;
-    mazeElement.style.gridTemplateRows = `repeat(${height}, 40px)`;
+    mazeElement.style.gridTemplateColumns = `repeat(${width}, 20px)`;
+    mazeElement.style.gridTemplateRows = `repeat(${height}, 20px)`;
 
     goal = { x: width - 2, y: height - 2 };
     seen = generateSeenArray(width, height);
@@ -110,22 +67,20 @@ function setupMazeData(mazeIndex) {
 
 document.addEventListener("keydown", (e) => {
 
-  const keys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
-  if (keys.includes(e.key)) e.preventDefault();
+    const keys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+    if (keys.includes(e.key)) e.preventDefault();
 
-  switch (e.key) {
-    case "ArrowUp": 
-    case "w": movePlayer(0, -1); break;
+    switch (e.key) {
+        case "ArrowUp":
+        case "w": movePlayer(0, -1); break;
 
-    case "ArrowDown": 
-    case "s": movePlayer(0, 1); break;
+        case "ArrowDown":
+        case "s": movePlayer(0, 1); break;
 
-    case "ArrowLeft": 
-    case "a": movePlayer(-1, 0); break;
+        case "ArrowLeft":
+        case "a": movePlayer(-1, 0); break;
 
-    case "ArrowRight": 
-    case "d": movePlayer(1, 0); break;
-  }
+        case "ArrowRight":
+        case "d": movePlayer(1, 0); break;
+    }
 });
-
-setupMazeData(2);
